ab9481a7d9fd03962669b622641d605c391fa413
Benjamin-ev <benjamin.evain4@gmail.com>
2022-09-08 15:54:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors({
 app.use(express.json())
 app.use(express.static(path.join(__dirname, process.env.FRONT_URL)))
 
-const routes = ['users', 'groups', 'modify', 'new', 'profile']
+const routes = ['users', 'groups', 'modify', 'profile', 'reset']
 app.get('/', (req,res) => {
   res.sendFile(path.join(__dirname, process.env.FRONT_URL+'/index.html')) // Route principale, page d'accueil
 })
